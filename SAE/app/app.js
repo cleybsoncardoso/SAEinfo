51b02d70e05d112dfda55f798e5efc512a3aaa8b
(Cleybson <cleybsonc@gmail.com>
2016-06-09 21:16:15 -0300

    iniciando tela login

--- SAE/app/app.js
+++ SAE/app/app.js
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
+import {LoginPage} from './pages/login/login';
 
 
 @Component({
@@ -13,7 +13,7 @@ export class MyApp {
   }
 
   constructor(platform) {
-    this.rootPage = HomePage;
+    this.rootPage = LoginPage;
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
