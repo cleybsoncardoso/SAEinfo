cc9c2b337b51813001b85f3944f32d4e29cf98cd
(Cleybson <cleybsonc@gmail.com>
2016-06-11 00:40:01 -0300

    removido projeto anterior

--- saeproject/app/pages/principal/principal.js
+++ saeproject/app/pages/principal/principal.js
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController,Page} from 'ionic-angular';
 
 /*
   Generated class for the PrincipalPage page.
