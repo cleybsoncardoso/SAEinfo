8c2fa836782fe46ffa249b51e7769511321a2361
(Cleybson <cleybsonc@gmail.com>
2016-06-17 00:44:40 -0300

    alterando alterar senha

--- saeproject/app/app.js
+++ saeproject/app/app.js
@@ -3,6 +3,7 @@ import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 import {PacientesPage} from './pages/pacientes/pacientes';
+import {AlterarSenhaPage} from './pages/alterar-senha/alterar-senha';
 
 
 @Component({
@@ -17,6 +18,8 @@ export class MyApp {
   constructor(platform) {
     this.rootPage = HomePage;
     this.paciente = PacientesPage;
+    this.home = HomePage;
+    this.alterarsenha = AlterarSenhaPage;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
