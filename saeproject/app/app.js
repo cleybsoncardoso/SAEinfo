903ba4b6bcf52369a59bdc61468bef88c22943d5
lucaspaiva <lukspaiva@hotmail.com>
2016-06-22 14:36:29 -0300

    alteracao na tela de cadastro do paciente

--- saeproject/app/app.js
+++ saeproject/app/app.js
@@ -1,18 +1,15 @@
-import {App, Platform, NavController} from 'ionic-angular';
-import {ViewChild} from '@angular/core';
+import {Platform, ionicBootstrap, NavController} from 'ionic-angular';
+import {ViewChild, Component} from '@angular/core';
 import {StatusBar} from 'ionic-native';
-import {LoginPage} from './pages/login/login';
+import {HomePage} from './pages/home/home';
 import {PacientesPage} from './pages/pacientes/pacientes';
 import {AlterarSenhaPage} from './pages/alterar-senha/alterar-senha';
-<<<<<<< HEAD
 import {AddPacientePage} from './pages/add-paciente/add-paciente';
-=======
->>>>>>> 8c2fa836782fe46ffa249b51e7769511321a2361
 
-@App({
+@Component({
   templateUrl: 'build/app.html',
   config: {},
-  queries: {nav: new ViewChild('rootNavController') }
+  queries: {nav: new ViewChild('rootNavController')}
 })
 export class MyApp {
 
@@ -21,18 +18,11 @@ export class MyApp {
   }
 
   constructor(platform) {
-<<<<<<< HEAD
-    this.login = LoginPage;
+    this.rootPage = HomePage;
     this.pacientes = PacientesPage;
     this.alterarsenha = AlterarSenhaPage;
     this.addPaciente = AddPacientePage;
-    this.rootPage = LoginPage;
-=======
-    this.rootPage = HomePage;
-    this.paciente = PacientesPage;
-    this.home = HomePage;
-    this.alterarsenha = AlterarSenhaPage;
->>>>>>> 8c2fa836782fe46ffa249b51e7769511321a2361
+    this.homePage = HomePage;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -46,7 +36,9 @@ export class MyApp {
   }
 
   sair(){
-    this.nav.setRoot(LoginPage);
+    this.nav.setRoot(HomePage);
   }
 
 }
+
+ionicBootstrap(MyApp);
