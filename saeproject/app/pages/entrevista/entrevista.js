6d1cffe8855f0a218e808668653dff61dab6909f
(Cleybson <cleybsonc@gmail.com>
2016-07-01 18:21:32 -0300

    ajeitando telas

--- saeproject/app/pages/entrevista/entrevista.js
+++ saeproject/app/pages/entrevista/entrevista.js
@@ -1,5 +1,6 @@
 import {NavController, Page} from 'ionic-angular';
 import {IdentificacaoPage} from '../../pages/identificacao/identificacao';
+import {AvaliacaoNeurologicaPage} from '../../pages/avaliacao-neurologica/avaliacao-neurologica';
 
 /*
   Generated class for the EntrevistaPage page.
@@ -17,6 +18,101 @@ export class EntrevistaPage {
 
   constructor(nav) {
     this.nav = nav;
+    this.myIcons = ["md-add"];
+    this.initiateIcons();
+    this.qtdeOutros = 0;
+    this.qtdeAlergias = 0;
+    this.qtdeVacinas = 0;
+
+  }
+  addAntecedente() {
+    //guardando o div pai
+    let divPai = document.getElementById("outrosAntecetendes");
+    //Criando o elemento DIV filho;
+    let divFilho = document.createElement("div");
+    //Definindo atributos ao campoFilho:
+    divFilho.setAttribute("id","antecedente"+this.qtdeOutros);
+    divFilho.setAttribute("class", "divitem4");
+    //Inserindo o elemento filho no pai:
+    divPai.appendChild(divFilho);
+    //Escrevendo algo no filho recém-criado:
+    document.getElementById("antecedente"+this.qtdeOutros).innerHTML = "<input class='divitem2' type='text' id='campoAntecedente"+this.qtdeOutros+"'></input>";
+    this.qtdeOutros++;
+  }
+
+  /**Função que Remove um campo na relação de antecedentes*/
+  removerAntecedente() {
+      //só remove se já ouver um campo adicionado
+      if(this.qtdeOutros > 0){
+        this.qtdeOutros--;
+        //Guardando o div pai
+        let divPai = document.getElementById("outrosAntecetendes");
+        let text = "antecedente"+this.qtdeOutros;
+        //Guardando o ultimo div filho criado
+        let divFilho = document.getElementById(text);
+        //Removendo o ultimo DIV do nó-pai:
+        divPai.removeChild(divFilho);
+      }
+  }
+
+  /**Função que adiciona um campo na relação de Alergia*/
+  addAlergia() {
+    //guardando o div pai
+    let divPai = document.getElementById("alergias");
+    //Criando o elemento DIV filho;
+    let divFilho = document.createElement("div");
+    //Definindo atributos ao campoFilho:
+    divFilho.setAttribute("id","alergia"+this.qtdeAlergias);
+    divFilho.setAttribute("class", "divitem4");
+    //Inserindo o elemento filho no pai:
+    divPai.appendChild(divFilho);
+    //Escrevendo algo no filho recém-criado:
+    document.getElementById("alergia"+this.qtdeAlergias).innerHTML = "<input class='divitem2' type='text' id='campoAlergia"+this.qtdeAlergias+"'></input>";
+    this.qtdeAlergias++;
+  }
+
+  /**Função que Remove um campo na relação de Alergia*/
+  removerAlergia() {
+      //só remove se já ouver um campo adicionado
+      if(this.qtdeAlergias > 0){
+        this.qtdeAlergias--;
+        //Guardando o div pai
+        let divPai = document.getElementById("alergias");
+        let text = "alergia"+this.qtdeAlergias;
+        //Guardando o ultimo div filho criado
+        let divFilho = document.getElementById(text);
+        //Removendo o ultimo DIV do nó-pai:
+        divPai.removeChild(divFilho);
+      }
+  }
+  addVacina() {
+    //guardando o div pai
+    let divPai = document.getElementById("vacinas");
+    //Criando o elemento DIV filho;
+    let divFilho = document.createElement("div");
+    //Definindo atributos ao campoFilho:
+    divFilho.setAttribute("id","vacina"+this.qtdeVacinas);
+    divFilho.setAttribute("class", "divitem4");
+    //Inserindo o elemento filho no pai:
+    divPai.appendChild(divFilho);
+    //Escrevendo algo no filho recém-criado:
+    document.getElementById("vacina"+this.qtdeVacinas).innerHTML = "<input class='divitem2' type='text' id='campoVacina"+this.qtdeVacinas+"'></input>";
+    this.qtdeVacinas++;
+  }
+
+  /**Função que Remove um campo na relação de vacina*/
+  removerVacina() {
+      //só remove se já ouver um campo adicionado
+      if(this.qtdeVacinas > 0){
+        this.qtdeVacinas--;
+        //Guardando o div pai
+        let divPai = document.getElementById("vacinas");
+        let text = "vacina"+this.qtdeVacinas;
+        //Guardando o ultimo div filho criado
+        let divFilho = document.getElementById(text);
+        //Removendo o ultimo DIV do nó-pai:
+        divPai.removeChild(divFilho);
+      }
   }
 
   cancel(){
@@ -25,6 +121,42 @@ export class EntrevistaPage {
   slide(passar){
     if(passar.deltaX>0){
       this.nav.setRoot(IdentificacaoPage);
+    }else if(passar.deltaX<0){
+      this.nav.setRoot(AvaliacaoNeurologicaPage);
     }
+}
+initiateIcons(){
+  var i = 0;
+  for(i=0;i<17;i++){
+    this.myIcons[i] = "md-add";
+  }
+}
+toggleGroup(id){
+  let grupo = document.getElementById("dados"+id);
+  this.toggleClose(id);
+  if(grupo.style.visibility == "visible"){
+    this.myIcons[id] = "md-add";
+    grupo.style.height = "0px";
+    grupo.style.visibility = "hidden";
+  }else{
+    grupo.style.visibility = "visible";
+    grupo.style.height = "auto";
+  }
+}
+
+toggleClose(id){
+  var i=0;
+  let grupo = document.getElementById("dados"+i);
+  while(grupo!=null){
+    if(i!=id){
+      grupo.style.height = "0px";
+      grupo.style.visibility = "hidden";
+      this.myIcons[i] = "md-add";
+    }else{
+      this.myIcons[i] = "md-remove";
+    }
+    i++;
+    grupo = document.getElementById("dados"+i);
+  }
 }
 }
