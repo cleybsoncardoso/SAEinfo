41284db3975c98eaa6c98481cff25a1066aa50a5
(Cleybson <cleybsonc@gmail.com>
2016-07-02 01:18:24 -0300

    terminando telas de fomulario

--- saeproject/app/pages/formulario/observacoes/observacoes.js
+++ saeproject/app/pages/formulario/observacoes/observacoes.js
@@ -17,6 +17,7 @@ export class ObservacoesPage {
 
   constructor(nav) {
     this.nav = nav;
+    this.qtdeObs = 0;
   }
   cancel(){
     this.nav.setRoot(PacientesPage);
@@ -26,6 +27,36 @@ export class ObservacoesPage {
       this.nav.setRoot(SegurancaFisicaPage);
     }
   }
-  
+
+  addObservacoes() {
+    //guardando o div pai
+    let divPai = document.getElementById("observacoes");
+    //Criando o elemento DIV filho;
+    let divFilho = document.createElement("div");
+    //Definindo atributos ao campoFilho:
+    divFilho.setAttribute("id","observacoes"+this.qtdeObs);
+    divFilho.setAttribute("class", "divitem4");
+    //Inserindo o elemento filho no pai:
+    divPai.appendChild(divFilho);
+    //Escrevendo algo no filho recém-criado:
+    document.getElementById("observacoes"+this.qtdeObs).innerHTML = "<input class='divitem2' type='text' id='campoObservacoes"+this.qtdeObs+"'></input>";
+    this.qtdeObs++;
+  }
+
+  /**Função que Remove um campo na relação de observacoes*/
+  removerObservacoes() {
+      //só remove se já ouver um campo adicionado
+      if(this.qtdeObs > 0){
+        this.qtdeObs--;
+        //Guardando o div pai
+        let divPai = document.getElementById("observacoes");
+        let text = "observacoes"+this.qtdeObs;
+        //Guardando o ultimo div filho criado
+        let divFilho = document.getElementById(text);
+        //Removendo o ultimo DIV do nó-pai:
+        divPai.removeChild(divFilho);
+      }
+  }
+
 
 }
