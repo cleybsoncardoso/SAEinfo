099eb02d88ec97f1c7b01727c0949064da4e0a7e
lucaspaiva <lukspaiva@hotmail.com>
2016-07-02 00:59:02 -0300

    correcao tela alimentacao

--- saeproject/app/pages/formulario/alimentacao-e-eliminacao-intestinal/alimentacao-e-eliminacao-intestinal.js
+++ saeproject/app/pages/formulario/alimentacao-e-eliminacao-intestinal/alimentacao-e-eliminacao-intestinal.js
@@ -8,7 +8,7 @@ import {NavController} from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/alimentacao-e-eliminacao-intestinal/alimentacao-e-eliminacao-intestinal.html',
+  templateUrl: 'build/pages/formulario/alimentacao-e-eliminacao-intestinal/alimentacao-e-eliminacao-intestinal.html',
 })
 export class AlimentacaoEEliminacaoIntestinalPage {
   static get parameters() {
@@ -18,4 +18,52 @@ export class AlimentacaoEEliminacaoIntestinalPage {
   constructor(nav) {
     this.nav = nav;
   }
+
+  cancel(){
+    this.nav.setRoot(PacientesPage);
+  }
+  slide(passar){
+    if(passar.deltaX>0){
+      this.nav.setRoot(IdentificacaoPage);
+    }else if(passar.deltaX<0){
+      this.nav.setRoot(AvaliacaoNeurologicaPage);
+    }
+  }
+
+  toggleGroup(id){
+    let grupo = document.getElementById("dados"+id);
+    let icone = document.getElementById("icone"+id);
+    this.toggleClose(id);
+    if(grupo.style.display == "block"){
+      grupo.style.display = "none";
+      icone.innerHTML = '+';
+    }else{
+      grupo.style.display = "block";
+      icone.innerHTML = '-';
+    }
+  }
+  toggleClose(id){
+    var i=0;
+    let grupo = document.getElementById("dados"+i);
+    let icone = document.getElementById("icone"+i);
+    while(grupo!=null){
+      console.log(i);
+      if(i!=id){
+        grupo.style.display = "none";
+        icone.innerHTML = '+';
+      }
+      i++;
+      icone = document.getElementById("icone"+i);
+      grupo = document.getElementById("dados"+i);
+    }
+  }
+  toggleVomito(){
+    let grupo = document.getElementById("vomito");
+    if(grupo.style.display == "block"){
+      grupo.style.display = "none";
+    }else{
+      grupo.style.display = "block";
+    }
+  }
+
 }
